0be632d59c2a7bf66176974d8f4aa87c4e28cee6
Qson <qson@qsondemacbook-pro-2.local>
2018-07-03 17:31:45 +0800

    仿系统导航条的，处理状态栏高度更改，导航条及界面不往下移的问题

--- QSSearchDemo/QSSearchView/QSMacro.h
+++ QSSearchDemo/QSSearchView/QSMacro.h
@@ -32,12 +32,20 @@
 #define IS_IPHONE_X (fabs((double)[[UIScreen mainScreen] bounds].size.height - (double)812)<DBL_EPSILON)
 
 #define kScreenWidth ([[UIScreen mainScreen] bounds].size.width)
-#define kScreenHeight ([[UIScreen mainScreen] bounds].size.height)
+#define kStatusBarHeight (([[UIApplication sharedApplication] statusBarFrame].size.height == kStatusBarDefHeight) ? kStatusBarDefHeight : (([[UIApplication sharedApplication] statusBarFrame].size.height == kStatusBarMaxHeight) ? kStatusBarDefHeight : 0.0f))
 
-#define kStatusBarHeight CGRectGetHeight([UIApplication sharedApplication].statusBarFrame)
+#define kScreenWidth ([[UIScreen mainScreen] bounds].size.width)
+#define kScreenHeight    (([[UIApplication sharedApplication] statusBarFrame].size.height > kStatusBarDefHeight) ? [UIScreen mainScreen].bounds.size.height - kStatusBarDefHeight : [UIScreen mainScreen].bounds.size.height)
+
 #define kSafeAreaBottom (IS_IPHONE_X ? 34 : 0)
 #define kSafeAreaTop (IS_IPHONE_X ? kStatusBarHeight : 0)
 #define kNavigationBarHeight (kStatusBarHeight + 44)
+
+// kStatusBar
+// 状态栏默认高度
+#define kStatusBarDefHeight IS_IPHONE_X ? 44.0f : 20.0f
+// 状态栏最大高度
+#define kStatusBarMaxHeight IS_IPHONE_X ? 64.0f : 40.0f
 
 #define QSColor(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 
